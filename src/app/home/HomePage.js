06162fbc8b1a50ac1316116f996967fdc97a4fa3
Tzu-Yin Hong <lili668668@gmail.com>
2018-07-27 22:59:37 +0800

    Make poinko gallery

--- src/app/home/HomePage.js
+++ src/app/home/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
 import poinkos from '../../assets/poinkos'
 
 const styles = theme => ({
@@ -11,7 +13,13 @@ function HomePage (props) {
   const { classes } = props
   return (
     <div>
-      {poinkos.map(poinko => (<img src={poinko.src} />))}
+      <GridList cellHeight={300} cols={4}>
+        {poinkos.map(poinko => (
+          <GridListTile key={poinko.src} cols={1}>
+            <img src={poinko.src} />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   )
 }
