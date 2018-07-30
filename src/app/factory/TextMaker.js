8fa96c87c6a1d597c87b550858198cfd20f73db1
Tzu-Yin Hong <lili668668@gmail.com>
2018-07-30 14:23:17 +0800

    Add set position function and set font size

--- src/app/factory/TextMaker.js
+++ src/app/factory/TextMaker.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import { compose, withState, withHandlers, lifecycle } from 'recompose'
 import Konva from 'konva'
-import { Stage, Layer, Image, Text } from 'react-konva'
+import { Stage, Layer, Image, Text, Rect } from 'react-konva'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Slider from '@material-ui/lab/Slider'
@@ -20,9 +20,19 @@ function TextMaker (props) {
     handleDownload,
     handleStageRef,
     handleTextChange,
-    handleFontSizeChange
+    handleFontSizeChange,
+    handleTextDragStart,
+    handleTextDragEnd,
+    handleResetTextPosition
   } = props
-  const { image, text, fontSize } = state
+  const {
+    image,
+    text,
+    fontSize,
+    textX,
+    textY,
+    textBorderColor
+  } = state
   const width = picture.width
   const height = picture.height
   return (
@@ -39,12 +49,25 @@ function TextMaker (props) {
           <Image image={image} />
           <Text
             width={width}
-            y={height - 100}
+            x={textX}
+            y={textY}
             text={text}
             align="center"
             fill="white"
             fontSize={fontSize}
             fontFamily="Noto Sans CJK TC, Microsoft JhengHei, Microsoft YaHei, sans-serif"
+            fontStyle="bold"
+            stroke="black"
+          />
+          <Rect
+            width={width}
+            height={fontSize}
+            x={textX}
+            y={textY}
+            stroke={textBorderColor}
+            onDragStart={handleTextDragStart}
+            onDragEnd={handleTextDragEnd}
+            draggable
           />
         </Layer>
       </Stage>
@@ -55,13 +78,18 @@ function TextMaker (props) {
       <div className={classes.slider}>
         <Slider value={fontSize} onChange={handleFontSizeChange} />
       </div>
+      <Button onClick={handleResetTextPosition}>
+        reset position
+      </Button>
     </div>
   )
 }
 
 TextMaker.propsTypes = {
   picture: PropTypes.string.isRequire,
-  defaultFontSize: PropTypes.number
+  defaultFontSize: PropTypes.number,
+  defaultTextX: PropTypes.number,
+  defaultTextY: PropTypes.number
 }
 
 const lifecycles = {
@@ -95,6 +123,21 @@ const handlers = {
   },
   handleFontSizeChange: ({ state, setState }) => (event, value) => {
     setState({ ...state, fontSize: value })
+  },
+  handleTextDragStart: ({ state, setState }) => (event) => {
+    setState({ ...state, textBorderColor: 'red' })
+  },
+  handleTextDragEnd: ({ state, setState, picture: { width, height } }) => (event) => {
+    let x = event.target.x()
+    let y = event.target.y()
+    if (x > width * 2) x = 0
+    if (x < 0 - width) x = 0
+    if (y > height) y = height - 100
+    if (y < 0) y = 0
+    setState({ ...state, textX: x, textY: y, textBorderColor: 'transparent' })
+  },
+  handleResetTextPosition: ({ state, setState, ...props }) => () =>{
+    setState({ ...state, ...(initialTextPosition(props)) })
   }
 }
 
@@ -102,7 +145,14 @@ const initialState = (props) => ({
   image: null,
   stageRef: null,
   text: '',
-  fontSize: props.defaultFontSize || 50
+  ...(initialTextPosition(props)),
+  fontSize: props.defaultFontSize || 50,
+  textBorderColor: 'transparent'
+})
+
+const initialTextPosition = (props) => ({
+  textX: props.defaultTextX || 0,
+  textY: props.defaultTextY || (props.picture.height - 100)
 })
 
 export default compose(
